8724ea08962401a18eef051c9c0faaa6c0e0976c
Caleb Hengeveld <crh14@sfu.ca>
2024-10-22 11:54:13 -0700

    Door done except for enter()

--- src/main/java/Door.java
+++ src/main/java/Door.java
@@ -1,15 +1,30 @@
 public class Door extends Mapsite {
+    private static int INITIAL_COUNT = 0;
     private RoomBlock r1;
     private RoomBlock r2;
     private int doorNr;
-    private static int doorCnt;
+    private static int doorCnt = INITIAL_COUNT;
 
-    public Door(RoomBlock input1, RoomBlock input2) throws Exception {
-        if(input1 == input2) {
+    public Door(RoomBlock input1, RoomBlock input2) /* throws Exception */ {
+        /* if(input1 == input2) {
             throw new Exception("r1 cannot == r2!\n");
-        } else {
+        } else { */
             this.r1 = input1;
             this.r2 = input2;
+        // }
+        setDoorNr(doorCnt);
+        doorCnt++;
+    }
+    public void enter() {
+        // 
+    }
+
+    private void setDoorNr(int number) throws Exception {
+        if(number >= 0) {
+            this.doorNr = number;
+        } else {
+            throw new Exception("Door number must be 0 or greater!\n");
         }
     }
+
 }
