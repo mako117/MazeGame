b61f823d4f5d4e5c3d4dd0d3301934da2f6adc8d
Caleb Hengeveld <crh14@sfu.ca>
2024-10-22 11:26:08 -0700

    Door created with exception throwing

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,11 +1,14 @@
 public class Board {
+    private RoomBlock startRoomBlock;
+    private RoomBlock endRoomBlock;
+
     public Board createBoard() {
 
     }
-    public void setStart(RoomBlock) {
-
+    public void setStart(RoomBlock thisRoomBlock) {
+        this.startRoomBlock = thisRoomBlock;
     }
-    public void setEnd(RoomBlock) {
-        
+    public void setEnd(RoomBlock thisRoomBlock) {
+        this.endRoomBlock = thisRoomBlock;
     }
 }
--- src/main/java/Door.java
+++ src/main/java/Door.java
@@ -4,8 +4,12 @@ public class Door extends Mapsite {
     private int doorNr;
     private static int doorCnt;
 
-    public Door(RoomBlock input1, RoomBlock input2) {
-        this.r1 = input1;
-        this.r2 = input2;
+    public Door(RoomBlock input1, RoomBlock input2) throws Exception {
+        if(input1 == input2) {
+            throw new Exception("r1 cannot == r2!\n");
+        } else {
+            this.r1 = input1;
+            this.r2 = input2;
+        }
     }
 }
