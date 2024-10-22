fc8c4a74b4101409970d2be0197ab5938d7229f9
alam <ala328@sfu.ca>
2024-10-22 11:50:13 -0700

    Change Enter to return boolean

--- src/main/java/Mapsite.java
+++ src/main/java/Mapsite.java
@@ -2,6 +2,7 @@
  * The MapSite class is a component of the Maze.
  * It is used by the Wall, Door, and Room classes.
  */
-public abstract class MapSite {
-    public abstract void enter();
+public abstract class Mapsite {
+
+    public abstract boolean enter();
 }
