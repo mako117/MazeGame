5f94dbd2cbedae94cfb52eeebf9fe9d0479436d3
Rodrigo Arce Diaz <ria12@sfu.ca>
2024-10-22 10:51:39 -0700

    'Wall.java' class created

---
+++ src/main/java/Wall.java
@@ -0,0 +1,32 @@
+/**
+ * The Wall class is used to create a wall and its interactions
+ */
+class Wall extends Mapsite {
+    /**
+     * Creates a wall and assigns the wall a unique number
+     */
+    Wall() {
+        _wallNr = _wallCnt++;
+        System.out.println("creating Wall #" + new Integer(_wallNr).toString());
+    }
+
+    /**
+     * Enter a wall
+     */
+    public void enter() {
+        System.out.println("You are trying to enter Wall #" + _wallNr);
+    }
+
+    /**
+     * Return the wall number as a string
+     * 
+     * @return a wall number as a String
+     */
+    public String toString() {
+        return "Wall #" + new Integer(_wallNr).toString();
+    }
+
+    private int _wallNr;
+    private static int _wallCnt = 1;
+
+}
