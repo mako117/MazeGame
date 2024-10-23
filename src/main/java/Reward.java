1a016d1abf9d4bbe5421f8b29f10669c3d137c08
Shiqi Zhong <sza181@sfu.ca>
2024-10-23 08:08:27 +0800

    Reward class

---
+++ src/main/java/Reward.java
@@ -0,0 +1,33 @@
+
+public class Reward {
+	private int x;
+	private int y;
+	private int score;
+	
+	Reward(int x, int y, int s){
+		this.x = x;
+		this.y = y;
+		this.score = s;
+	}
+	
+	void Set_score(int s) {
+		this.score = s;
+	}
+	
+	void Set_poistion(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	int Xposition() {
+		return x;
+	}
+	
+	int Yposition() {
+		return y;
+	}
+	
+	int getPoint() {
+		return score;
+	}
+}
