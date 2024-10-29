106da1fcb33c03c82805d50bcc43c8cb7a70264c
Alicia Lam <alicia_lam_2@sfu.ca>
2024-10-29 16:32:34 -0700

    implement gdx ApplicationListener

--- src/main/java/MazeGame.java
+++ src/main/java/MazeGame.java
@@ -5,10 +5,82 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 
-public class MazeGame {
-    public static void main(String args[]) {
-        Character Rock = new Character();
-        Board gameBoard = new Board();
-        gameBoard.createBoard();
+public class MazeGame implements ApplicationListener{
+//    public static void main(String args[]) {
+//        Character Rock = new Character();
+//        Board gameBoard = new Board();
+//        gameBoard.createBoard();
+//    }
+
+    /**
+     * Instantiate game objects
+     */
+    public void create() {
+
     }
+
+    /**
+     * Resize window (follow character view/camera)
+     * @param i
+     * @param i1
+     */
+    public void resize(int i, int i1) {
+
+    }
+
+    /**
+     * Draw the game on the window
+     */
+    public void render() {
+
+    }
+
+    /**
+     * Take user input
+     */
+    private void input() {
+
+    }
+
+
+    /**
+     * Apply game logic
+     */
+    private void logic() {
+
+    }
+
+    /**
+     * Draw to window
+     */
+    private void draw(){
+
+    }
+
+
+
+
+    // <-- NOTE: all the methods below need to be overridden from ApplicationListener -->
+
+    /**
+     *
+     */
+    public void pause() {
+
+    }
+
+    /**
+     *
+     */
+    public void resume() {
+
+    }
+
+    /**
+     *
+     */
+    public void dispose() {
+
+    }
+
 }
