063714a87ab443e5ed409559dd4973ecf20c14c7
Alicia Lam <alicia_lam_2@sfu.ca>
2024-10-29 16:47:31 -0700

    Add constructors to BonusPunishments

--- src/main/java/BonusPunishments.java
+++ src/main/java/BonusPunishments.java
@@ -2,5 +2,22 @@
  * The bonus punishment in the maze game.
  */
 public class BonusPunishments extends Punishments{
-    
+    private int bonusNr;
+    private static int bonusCnt = 1;
+    private int starttime;
+    private int endtime;
+
+    BonusPunishments(){
+        super();
+        bonusNr = bonusCnt++;
+        starttime = -1;
+        endtime = -1;
+    }
+
+    BonusPunishments(int inputX, int inputY, int inputScore, int startT, int endT){
+        super(inputX, inputY, inputScore);
+        this.starttime = startT;
+        this.endtime = endT;
+    }
+
 }
