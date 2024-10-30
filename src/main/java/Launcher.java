b7ddc75c90bd29cf6dc374264e8e35363a81361d
Alicia Lam <alicia_lam_2@sfu.ca>
2024-10-29 19:38:42 -0700

    Rename main to launcher

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,16 @@
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class Main {
+    // initial class (may remove/change)
+    public static void main(String args[]) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setIdleFPS(60);
+        config.useVsync(true);
+        config.setTitle("PLACEHOLDER");
+
+        config.setWindowedMode(600,400);
+
+        new Lwjgl3Application(new MazeGame(), config);
+    }
+}
--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    // initial class (may remove/change)
-    public static void main(String[] args){
-        System.out.println("test");
-    }
-}
