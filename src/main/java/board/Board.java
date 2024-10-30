19361d8acf423d0e73a28be4c3d13251a8ea8a00
Alicia Lam <alicia_lam_2@sfu.ca>
2024-10-30 00:14:21 -0700

    Change Board to use arraylist

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -1,76 +1,92 @@
 package board;
 import directions.Direction;
 
+import java.util.ArrayList;
+
 public class Board {
     private RoomBlock startRoomBlock;
     private RoomBlock endRoomBlock;
-    private Block [][] array;
+    private ArrayList<ArrayList<Block>> array;
+	private int height = 10;
+	private int width = 15;
 
     public void createBoard() {
-    	array = new Block[3][3];
-    	array[0][0] = new RoomBlock(0,0);
-    	array[0][1] = new RoomBlock(0,1);
-    	array[0][2] = new RoomBlock(0,2);
-    	array[1][0] = new RoomBlock(1,0);
-    	array[1][1] = new BarrierBlock(1,1);
-    	array[1][2] = new RoomBlock(1,2);
-    	array[2][0] = new RoomBlock(2,0);
-    	array[2][1] = new RoomBlock(2,1);
-    	array[2][1] = new RoomBlock(2,2);
-    	
-    	Door d1 = new Door(array[0][0],array[0][1]);
-    	Door d2 = new Door(array[0][1],array[0][2]);
-    	Door d3 = new Door(array[0][2],array[1][2]);
-    	Door d4 = new Door(array[1][2],array[2][2]);
-    	Door d5 = new Door(array[2][2],array[2][1]);
-    	Door d6 = new Door(array[2][1],array[2][0]);
-    	Door d7 = new Door(array[2][0],array[1][0]);
-    	Door d8 = new Door(array[1][0],array[0][0]);
-    	
-    	array[0][0].setSide(Direction.North, d8);
-    	array[0][0].setSide(Direction.South, new Wall());
-    	array[0][0].setSide(Direction.West, new Wall());
-    	array[0][0].setSide(Direction.East, d1);
-    	
-    	array[0][1].setSide(Direction.North, new Wall());
-    	array[0][1].setSide(Direction.South, new Wall());
-    	array[0][1].setSide(Direction.West, d1);
-    	array[0][1].setSide(Direction.East, d2);
-    	
-    	array[0][2].setSide(Direction.North, d3);
-    	array[0][2].setSide(Direction.South, new Wall());
-    	array[0][2].setSide(Direction.West, d2);
-    	array[0][2].setSide(Direction.East, new Wall());
-    	
-    	array[1][0].setSide(Direction.North, d7);
-    	array[1][0].setSide(Direction.South, d8);
-    	array[1][0].setSide(Direction.West, new Wall());
-    	array[1][0].setSide(Direction.East, new Wall());
-    	
-    	array[1][1].setSide(Direction.North, new Wall());
-    	array[1][1].setSide(Direction.South, new Wall());
-    	array[1][1].setSide(Direction.West, new Wall());
-    	array[1][1].setSide(Direction.East, new Wall());
-    	
-    	array[1][2].setSide(Direction.North, d4);
-    	array[1][2].setSide(Direction.South, d3);
-    	array[1][2].setSide(Direction.West, new Wall());
-    	array[1][2].setSide(Direction.East, new Wall());
-    	
-    	array[2][0].setSide(Direction.North, new Wall());
-    	array[2][0].setSide(Direction.South, d7);
-    	array[2][0].setSide(Direction.West, new Wall());
-    	array[2][0].setSide(Direction.East, d6);
-    	
-    	array[2][1].setSide(Direction.North, new Wall());
-    	array[2][1].setSide(Direction.South, new Wall());
-    	array[2][1].setSide(Direction.West, d6);
-    	array[2][1].setSide(Direction.East, d5);
-    	
-    	array[2][2].setSide(Direction.North, new Wall());
-    	array[2][2].setSide(Direction.South, d4);
-    	array[2][2].setSide(Direction.West, d5);
-    	array[2][2].setSide(Direction.East, new Wall());
+		array = new ArrayList<ArrayList<Block>>(width);
+		for(int i = 0; i < width; i++){
+			array.add(new ArrayList<Block>(height));
+		}
+		for(int i = 0; i < width; i++){
+			for(int j = 0; j < height; j++){
+				array.get(i).set(j, new RoomBlock());
+			}
+		}
+		// TODO: add walls and "space" wall surrounding board
+
+
+//    	array = new Block[3][3];
+//    	array[0][0] = new RoomBlock(0,0);
+//    	array[0][1] = new RoomBlock(0,1);
+//    	array[0][2] = new RoomBlock(0,2);
+//    	array[1][0] = new RoomBlock(1,0);
+//    	array[1][1] = new BarrierBlock(1,1);
+//    	array[1][2] = new RoomBlock(1,2);
+//    	array[2][0] = new RoomBlock(2,0);
+//    	array[2][1] = new RoomBlock(2,1);
+//    	array[2][1] = new RoomBlock(2,2);
+//
+//    	Door d1 = new Door(array[0][0],array[0][1]);
+//    	Door d2 = new Door(array[0][1],array[0][2]);
+//    	Door d3 = new Door(array[0][2],array[1][2]);
+//    	Door d4 = new Door(array[1][2],array[2][2]);
+//    	Door d5 = new Door(array[2][2],array[2][1]);
+//    	Door d6 = new Door(array[2][1],array[2][0]);
+//    	Door d7 = new Door(array[2][0],array[1][0]);
+//    	Door d8 = new Door(array[1][0],array[0][0]);
+//
+//    	array[0][0].setSide(Direction.North, d8);
+//    	array[0][0].setSide(Direction.South, new Wall());
+//    	array[0][0].setSide(Direction.West, new Wall());
+//    	array[0][0].setSide(Direction.East, d1);
+//
+//    	array[0][1].setSide(Direction.North, new Wall());
+//    	array[0][1].setSide(Direction.South, new Wall());
+//    	array[0][1].setSide(Direction.West, d1);
+//    	array[0][1].setSide(Direction.East, d2);
+//
+//    	array[0][2].setSide(Direction.North, d3);
+//    	array[0][2].setSide(Direction.South, new Wall());
+//    	array[0][2].setSide(Direction.West, d2);
+//    	array[0][2].setSide(Direction.East, new Wall());
+//
+//    	array[1][0].setSide(Direction.North, d7);
+//    	array[1][0].setSide(Direction.South, d8);
+//    	array[1][0].setSide(Direction.West, new Wall());
+//    	array[1][0].setSide(Direction.East, new Wall());
+//
+//    	array[1][1].setSide(Direction.North, new Wall());
+//    	array[1][1].setSide(Direction.South, new Wall());
+//    	array[1][1].setSide(Direction.West, new Wall());
+//    	array[1][1].setSide(Direction.East, new Wall());
+//
+//    	array[1][2].setSide(Direction.North, d4);
+//    	array[1][2].setSide(Direction.South, d3);
+//    	array[1][2].setSide(Direction.West, new Wall());
+//    	array[1][2].setSide(Direction.East, new Wall());
+//
+//    	array[2][0].setSide(Direction.North, new Wall());
+//    	array[2][0].setSide(Direction.South, d7);
+//    	array[2][0].setSide(Direction.West, new Wall());
+//    	array[2][0].setSide(Direction.East, d6);
+//
+//    	array[2][1].setSide(Direction.North, new Wall());
+//    	array[2][1].setSide(Direction.South, new Wall());
+//    	array[2][1].setSide(Direction.West, d6);
+//    	array[2][1].setSide(Direction.East, d5);
+//
+//    	array[2][2].setSide(Direction.North, new Wall());
+//    	array[2][2].setSide(Direction.South, d4);
+//    	array[2][2].setSide(Direction.West, d5);
+//    	array[2][2].setSide(Direction.East, new Wall());
     }
     public void setStart(RoomBlock thisRoomBlock) {
         this.startRoomBlock = thisRoomBlock;
@@ -80,6 +96,7 @@ public class Board {
     }
 
     public Block getBlock(int x, int y) {
-        return array[x][y];
+		return array.get(x).get(y);
+//        return array[x][y];
     }
 }
