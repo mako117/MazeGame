01fcb75110f6d5fb143389776de11d03c9d9321a
Alicia Lam <alicia_lam_2@sfu.ca>
2024-10-29 19:43:47 -0700

    Create test window

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,7 +1,7 @@
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-public class Main {
+public class Launcher {
     // initial class (may remove/change)
     public static void main(String args[]) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
