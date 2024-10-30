b25f8c8f479298b61228757afc8582f12d003456
Shiqi Zhong <sza181@sfu.ca>
2024-10-31 04:19:29 +0800

    change reward

--- src/main/java/rewards/Reward.java
+++ src/main/java/rewards/Reward.java
@@ -5,30 +5,30 @@ public class Reward {
 	private int y;
 	private int score;
 	
-	Reward(int x, int y, int s){
+	public Reward(int x, int y, int s){
 		this.x = x;
 		this.y = y;
 		this.score = s;
 	}
 	
-	void Set_score(int s) {
+	public void Set_score(int s) {
 		this.score = s;
 	}
 	
-	void Set_poistion(int x, int y) {
+	public void Set_poistion(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
-	int Xposition() {
+	public int Xposition() {
 		return x;
 	}
 	
-	int Yposition() {
+	public int Yposition() {
 		return y;
 	}
 	
-	int getPoint() {
+	public int getPoint() {
 		return score;
 	}
 }
