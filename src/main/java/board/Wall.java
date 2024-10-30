2a5b5e6422735d5ccb714810ee54741665c2238c
Shiqi Zhong <sza181@sfu.ca>
2024-10-31 02:55:00 +0800

    change wall

--- src/main/java/board/Wall.java
+++ src/main/java/board/Wall.java
@@ -3,13 +3,18 @@ package board;
 /**
  * The board.Wall class is used to create a wall and its interactions
  */
-class Wall extends Mapsite {
+class Wall extends Block {
     /**
      * Creates a wall and assigns the wall a unique number
      */
     Wall() {
+    	super();
         _wallNr = _wallCnt++;
         System.out.println("creating board.Wall #" + _wallNr);
+    }
+    
+    Wall(int x, int y){
+    	super(x,y);
     }
 
     /**
