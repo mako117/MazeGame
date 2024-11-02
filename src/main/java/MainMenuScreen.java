4344ad53501fd1aa784c4f30d8300f829cb57845
Shiqi Zhong <sza181@sfu.ca>
2024-11-02 11:44:44 +0800

    change MainMenuScreen

--- src/main/java/MainMenuScreen.java
+++ src/main/java/MainMenuScreen.java
@@ -12,11 +12,8 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MainMenuScreen implements Screen {
 	final MazeGame game;
-	
 	private SpriteBatch batch;
 	private BitmapFont font;
-	private Viewport viewport;
-	private Camera camera;
 
 	public MainMenuScreen(final MazeGame game) {
 		this.game = game;
@@ -25,25 +22,17 @@ public class MainMenuScreen implements Screen {
 	public void render(float delta) {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
-		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        viewport = new StretchViewport(2000, 3000, camera);
-        camera.update();
 		
-		//font.setUseIntegerPositions(false);
-		//font.getData().setScale(viewport.getWorldHeight()/Gdx.graphics.getHeight());
-		
-		//System.out.println("111 " + viewport.getWorldHeight() + " 2222 " + Gdx.graphics.getHeight());
 		ScreenUtils.clear(Color.BLACK);
 		
-		//viewport.apply();
-		batch.setProjectionMatrix(viewport.getCamera().combined);
+		
 
 		batch.begin();
 		//draw text. Remember that x and y are in meters
-		font.draw(batch, "Hello", camera.position.x, camera.position.y);
-		font.draw(batch, "Hello", camera.position.x, camera.position.y + 10);
-		//font.draw(batch, "Welcome to Drop!!! ", -0.5f,0);
-		//font.draw(batch, "Tap anywhere to begin!", -0.5f,-0.5f);
+		font.getData().setScale(2, 2);
+		font.draw(batch, "Game", Gdx.graphics.getWidth() / 2 - 20, Gdx.graphics.getHeight() / 2 + 40);
+		font.getData().setScale(3, 3);
+		font.draw(batch, "Click to Start", Gdx.graphics.getWidth() / 2 - 100, Gdx.graphics.getHeight() / 2 - 40);
 		batch.end();
 
 		if (Gdx.input.isTouched()) {
