12e744de5bd66ebdd09f5011102a68ff7a478b08
Shiqi Zhong <sza181@sfu.ca>
2024-11-02 10:45:11 +0800

    change end screen

--- src/main/java/EndScreen.java
+++ src/main/java/EndScreen.java
@@ -1,10 +1,14 @@
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class EndScreen implements Screen{
 	
@@ -14,7 +18,8 @@ public class EndScreen implements Screen{
 	
 	private SpriteBatch batch;
 	private BitmapFont font;
-	private FitViewport viewport;
+	private Viewport viewport;
+	private Camera camera;
 	
 	public EndScreen(MazeGame game, int score, float time) {
 		this.game = game;
@@ -33,39 +38,32 @@ public class EndScreen implements Screen{
 		// TODO Auto-generated method stub
 		batch = new SpriteBatch();
 		font = new BitmapFont();
-		viewport = new FitViewport(8,5);
-		
-		font.setUseIntegerPositions(false);
-		font.getData().setScale(viewport.getWorldHeight()/Gdx.graphics.getHeight());
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        viewport = new StretchViewport(2000, 3000, camera);
+        camera.update();
 		
 		//System.out.println("111 " + viewport.getWorldHeight() + " 2222 " + Gdx.graphics.getHeight());
 		ScreenUtils.clear(Color.BLACK);
 		
 		//viewport.apply();
 		batch.setProjectionMatrix(viewport.getCamera().combined);
-
-		batch.begin();
-		//draw text. Remember that x and y are in meters
-		
-		
 		
 		if(score > 0) {
-			font.draw(batch, "Game Win, End Screen ", -0.5f,0);
-			font.draw(batch, "Tap anywhere to try again begin!", -0.5f,-0.5f);
+			batch.begin();
+			font.draw(batch, "You Win", camera.position.x, camera.position.y);
+			font.draw(batch, "Click to play again", camera.position.x, camera.position.y - 100);
 			batch.end();
-			if (Gdx.input.isTouched()) {
-				game.setScreen(new GameScreen(game));
-				dispose();
-			}
 		}
 		else {
-			font.draw(batch, "Game Loss, End Screen ", -0.5f,0);
-			font.draw(batch, "Tap anywhere to try again begin!", -0.5f,-0.5f);
+			batch.begin();
+			font.draw(batch, "You lose", camera.position.x, camera.position.y);
+			font.draw(batch, "Click to try again", camera.position.x, camera.position.y - 100);
 			batch.end();
-			if (Gdx.input.isTouched()) {
-				game.setScreen(new GameScreen(game));
-				dispose();
-			}
+		}
+		
+		if (Gdx.input.isTouched()) {
+			game.setScreen(new GameScreen(game));
+			dispose();
 		}
 		
 		
@@ -98,7 +96,8 @@ public class EndScreen implements Screen{
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+		batch.dispose();
+		font.dispose();
 	}
 
 }
