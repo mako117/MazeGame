51be37368abc5389fac7cd2349520c1d6342eece
Shiqi Zhong <sza181@sfu.ca>
2024-11-03 02:56:40 +0800

    change mainmenuscreen

--- src/main/java/MainMenuScreen.java
+++ src/main/java/MainMenuScreen.java
@@ -22,7 +22,13 @@ public class MainMenuScreen implements Screen {
 	
 	private Skin skin;
 	private Button startbutton;
+	private Button exitbutton;
 	private Stage stage;
+	
+	int change_x = 0;
+	int change_y = 0;
+	int middle_x = Gdx.graphics.getWidth() / 2;
+	int middle_y = Gdx.graphics.getHeight() / 2;
 
 	public MainMenuScreen(final MazeGame game) {
 		this.game = game;
@@ -33,10 +39,13 @@ public class MainMenuScreen implements Screen {
 		stage = new Stage(new ScreenViewport());
 		Gdx.input.setInputProcessor(stage);
 		
+		change_x = -120;
+		change_y = -100;
+		
 		skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
 		startbutton = new TextButton("Start", skin, "small");
 		startbutton.setSize(Gdx.graphics.getWidth() / 12 * 2, Gdx.graphics.getHeight() / 12);
-		startbutton.setPosition(Gdx.graphics.getWidth() / 2 - 120, Gdx.graphics.getHeight() / 2 - 100);
+		startbutton.setPosition(middle_x + change_x, middle_y + change_y);
 		
 		// listener for touch button
 		startbutton.addListener(new ChangeListener() {
@@ -47,17 +56,37 @@ public class MainMenuScreen implements Screen {
 			
 		});
 		stage.addActor(startbutton);
+		
+		change_x = -70;
+		change_y = -180;
+		
+		exitbutton = new TextButton("Exit", skin, "small");
+		exitbutton.setSize(Gdx.graphics.getWidth() / 12, Gdx.graphics.getHeight() / 12);
+		exitbutton.setPosition(middle_x + change_x, middle_y + change_y);
+		
+		// listener for touch button
+		exitbutton.addListener(new ChangeListener() {
+					public void changed(ChangeEvent event, Actor actor) {
+						Gdx.app.exit();
+						dispose();
+					}
+			
+		});
+		stage.addActor(exitbutton);
 	}
 	
 	public void render(float delta) {
 		
 		ScreenUtils.clear(Color.BLACK);
 
+		change_x = -100;
+		change_y = 150;
+		
 		batch.begin();
 		//draw text. Remember that x and y are in meters
 		batch.draw(backgroundTexture, 0, 0);
 		font.getData().setScale(2, 2);
-		font.draw(batch, "Game", Gdx.graphics.getWidth() / 2 - 100, Gdx.graphics.getHeight() / 2 + 150);
+		font.draw(batch, "Game", middle_x + change_x, middle_y + change_y);
 		batch.end();
 		
 		//draw the button
