8671ef795ecde25a3113770bcadb878fff0db2b0
Shiqi Zhong <sza181@sfu.ca>
2024-11-04 11:26:34 +0800

    new page for help

--- src/main/java/MainMenuScreen.java
+++ src/main/java/MainMenuScreen.java
@@ -25,16 +25,21 @@ public class MainMenuScreen extends ScreenAdapter {
 	private Button startbutton;
 	private Button exitbutton;
 	private Button helpbutton;
+	private Button helppage1button;
+	private Button helppage2button;
 	private Button helpbackbutton;
 	private Stage stage1;
 	private Stage stage2;
+	private Stage stage3;
 	
 	private int change_x = 0;
 	private int change_y = 0;
 	private int middle_x = Gdx.graphics.getWidth() / 2;
 	private int middle_y = Gdx.graphics.getHeight() / 2;
 	
-	private boolean pause = false;
+	private boolean helppage = false;
+	private boolean helppage1 = false;
+	private boolean helppage2 = false;
 
 	public MainMenuScreen(final MazeGame game) {
 		this.game = game;
@@ -44,6 +49,7 @@ public class MainMenuScreen extends ScreenAdapter {
 		
 		stage1 = new Stage(new ScreenViewport());
 		stage2 = new Stage(new ScreenViewport());
+		stage3 = new Stage(new ScreenViewport());
 		
 		
 		change_x = -150;
@@ -91,11 +97,46 @@ public class MainMenuScreen extends ScreenAdapter {
 		// listener for touch button
 		helpbutton.addListener(new ChangeListener() {
 					public void changed(ChangeEvent event, Actor actor) {
-						pause = true;
+						helppage = true;
+						helppage1 = true;
 					}
 			
 		});
 		stage1.addActor(helpbutton);
+		
+		change_x = -450;
+		change_y = -300;
+		
+		helppage1button = new TextButton("Page 1", skin);
+		helppage1button.setSize(Gdx.graphics.getWidth() / 10 * 2, Gdx.graphics.getHeight() / 10);
+		helppage1button.setPosition(middle_x + change_x, middle_y + change_y);
+		
+		// listener for touch button
+		helppage1button.addListener(new ChangeListener() {
+					public void changed(ChangeEvent event, Actor actor) {
+						helppage1 = true;
+						helppage2 = false;
+					}
+			
+		});
+		stage3.addActor(helppage1button);
+		
+		change_x = 300;
+		change_y = -300;
+		
+		helppage2button = new TextButton("Page 2", skin);
+		helppage2button.setSize(Gdx.graphics.getWidth() / 10 * 2, Gdx.graphics.getHeight() / 10);
+		helppage2button.setPosition(middle_x + change_x, middle_y + change_y);
+		
+		// listener for touch button
+		helppage2button.addListener(new ChangeListener() {
+					public void changed(ChangeEvent event, Actor actor) {
+						helppage1 = false;
+						helppage2 = true;
+					}
+			
+		});
+		stage2.addActor(helppage2button);
 		
 		change_x = -150;
 		change_y = -300;
@@ -107,46 +148,79 @@ public class MainMenuScreen extends ScreenAdapter {
 		// listener for touch button
 		helpbackbutton.addListener(new ChangeListener() {
 					public void changed(ChangeEvent event, Actor actor) {
-						pause = false;
+						helppage = false;
+						helppage2 = false;
 					}
 			
 		});
-		stage2.addActor(helpbackbutton);
+		stage3.addActor(helpbackbutton);
 	}
 
 	public void render(float delta) {
 		
 		ScreenUtils.clear(Color.BLACK);
 		
-		if(pause) {
-			Gdx.input.setInputProcessor(stage2);
-			batch.begin();
-			
-			change_x = -180;
-			change_y = 300;
-			//draw text. Remember that x and y are in meters
-			batch.draw(backgroundTexture, 0, 0);
-			font.getData().setScale(2, 2);
-			font.draw(batch, "How to play", middle_x + change_x, middle_y + change_y);
-			
-			change_x = -(middle_x) + 50;
-			change_y = 200;
-			font.getData().setScale(1,1);;
-			font.draw(batch, "Movement: Use 'W', 'A', 'S', 'D' to move in the game", middle_x + change_x, middle_y + change_y);
-			
-			change_y = 100;
-			font.draw(batch, "Stop: Use 'Ese' can stop in the game", middle_x + change_x, middle_y + change_y);
-			
-			change_y = 0;
-			font.draw(batch, "Win the Game: Collect all the rewards show on the board and go to the end block", middle_x + change_x, middle_y + change_y);
-			
-			change_y = -100;
-			font.draw(batch, "Loss the Game: Monster catch you or point lower than 0", middle_x + change_x, middle_y + change_y);
-			
-			batch.end();
-			
-			stage2.act();
-			stage2.draw();
+		if(helppage) {
+			if(helppage1) {
+				Gdx.input.setInputProcessor(stage2);
+				batch.begin();
+				
+				change_x = -180;
+				change_y = 300;
+				//draw text. Remember that x and y are in meters
+				batch.draw(backgroundTexture, 0, 0);
+				font.getData().setScale(2, 2);
+				font.draw(batch, "How to play", middle_x + change_x, middle_y + change_y);
+				
+				change_x = -(middle_x) + 50;
+				change_y = 200;
+				font.getData().setScale(1,1);;
+				font.draw(batch, "Movement: Use 'W', 'A', 'S', 'D' to move in the game page 1", middle_x + change_x, middle_y + change_y);
+				
+				change_y = 100;
+				font.draw(batch, "Stop: Use 'Ese' can stop in the game", middle_x + change_x, middle_y + change_y);
+				
+				change_y = 0;
+				font.draw(batch, "Win the Game: Collect all the rewards show on the board and go to the end block", middle_x + change_x, middle_y + change_y);
+				
+				change_y = -100;
+				font.draw(batch, "Loss the Game: Monster catch you or point lower than 0", middle_x + change_x, middle_y + change_y);
+				
+				batch.end();
+				
+				stage2.act();
+				stage2.draw();
+			}
+			else if(helppage2) {
+				Gdx.input.setInputProcessor(stage3);
+				batch.begin();
+				
+				change_x = -180;
+				change_y = 300;
+				//draw text. Remember that x and y are in meters
+				batch.draw(backgroundTexture, 0, 0);
+				font.getData().setScale(2, 2);
+				font.draw(batch, "How to play", middle_x + change_x, middle_y + change_y);
+				/*
+				change_x = -(middle_x) + 50;
+				change_y = 200;
+				font.getData().setScale(1,1);;
+				font.draw(batch, "Movement: Use 'W', 'A', 'S', 'D' to move in the game page 2", middle_x + change_x, middle_y + change_y);
+				
+				change_y = 100;
+				font.draw(batch, "Stop: Use 'Ese' can stop in the game", middle_x + change_x, middle_y + change_y);
+				
+				change_y = 0;
+				font.draw(batch, "Win the Game: Collect all the rewards show on the board and go to the end block", middle_x + change_x, middle_y + change_y);
+				
+				change_y = -100;
+				font.draw(batch, "Loss the Game: Monster catch you or point lower than 0", middle_x + change_x, middle_y + change_y);
+				*/
+				batch.end();
+				
+				stage3.act();
+				stage3.draw();
+			}
 		}
 		else {
 			Gdx.input.setInputProcessor(stage1);
