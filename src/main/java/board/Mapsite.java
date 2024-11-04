91257b7c9533045d6802179b3c5887e8ec42ee67
alam <ala328@sfu.ca>
2024-11-04 11:38:27 -0800

    Remove door and mapsite

--- src/main/java/board/Mapsite.java
+++
@@ -1,9 +0,0 @@
-package board;
-
-/**
- * The MapSite class is a component of the Maze.
- * It is used by the board.Wall, board.Door, and Room classes.
- */
-public abstract class Mapsite {
-    public abstract boolean enter();
-}
