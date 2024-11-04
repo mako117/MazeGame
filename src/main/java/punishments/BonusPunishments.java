ffac9246172bba43e675f65e6950a8e043b85a14
Caleb Hengeveld <crh14@sfu.ca>
2024-11-04 14:29:18 -0800

    JavaDoc for punishments

--- src/main/java/punishments/BonusPunishments.java
+++ src/main/java/punishments/BonusPunishments.java
@@ -9,22 +9,40 @@ public class BonusPunishments extends Punishments{
     private int starttime;
     private int endtime;
 
+    /**
+     * Initializes the BonusPunishment's position, starttime, and endttime to -1, and sets the <score> to 0.
+     */
     public BonusPunishments(){
         super();
         starttime = -1;
         endtime = -1;
     }
-
+    /**
+     * Initializes the BonusPunishment's <x> and <y>-coordinates, <score>, <PunishmentsTexture>, <starttime>, and <endtime>.
+     * @param inputX    Input for the punishment's x-coordinate.
+     * @param inputY    Input for the punishment's y-coordinate.
+     * @param inputScore    Input for the punishment's score. 
+     * @param inputTexture  Input for the punishment's texture.
+     * @param startT    Input for the punishment's start time.
+     * @param endT  Input for the punishment's end time.
+     */
     public BonusPunishments(int inputX, int inputY, int inputScore, TextureRegion inputTexture, int startT, int endT){
         super(inputX, inputY, inputScore, inputTexture);
         this.starttime = startT;
         this.endtime = endT;
     }
 
+    /**
+     * Gets the punishment's <starttime>.
+     * @return  <starttime>.
+     */
     public int getStartTime() {
         return starttime;
     }
-
+    /**
+     * Gets the punishment's <endtime>.
+     * @return  <endtime>.
+     */
     public int  getEndTime(){
         return endtime;
     }
--- src/main/java/punishments/NormalPunishments.java
+++ src/main/java/punishments/NormalPunishments.java
@@ -6,7 +6,14 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  * The standard punishment in the maze game.
  */
 public class NormalPunishments extends Punishments{
-	
+
+    /**
+     * Initializes the NormalPunishment's <x> and <y>-coordinates, <score>, and <PunishmentsTexture>.
+     * @param inputX    Input for the punishment's x-coordinate.
+     * @param inputY    Input for the punishment's y-coordinate.
+     * @param inputScore    Input for the punishment's score. 
+     * @param inputTexture  Input for the punishment's texture.
+     */
     public NormalPunishments(int inputX, int inputY, int inputScore, TextureRegion inputTexture){
         super(inputX, inputY, inputScore, inputTexture);
     }
