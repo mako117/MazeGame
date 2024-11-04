562f044b0cdf406ee919d65c4a4dbc7146594781
Shiqi Zhong <sza181@sfu.ca>
2024-11-04 17:05:07 +0800

    add page for help

--- src/main/java/MainMenuScreen.java
+++ src/main/java/MainMenuScreen.java
@@ -26,6 +26,8 @@ public class MainMenuScreen extends ScreenAdapter {
 	private TextureRegion RpunishmentTex;
 	private TextureRegion BrewardTex;
 	private TextureRegion BpunishmentTex;
+	private TextureRegion movingEnemyTex;
+	private TextureRegion patrollingEnemeyTex;
 	
 	private Skin skin;
 	private Button startbutton;
@@ -33,10 +35,12 @@ public class MainMenuScreen extends ScreenAdapter {
 	private Button helpbutton;
 	private Button helppage1button;
 	private Button helppage2button;
+	private Button helppage3button;
 	private Button helpbackbutton;
 	private Stage stage1;
 	private Stage stage2;
 	private Stage stage3;
+	private Stage stage4;
 	
 	private int change_x = 0;
 	private int change_y = 0;
@@ -46,6 +50,7 @@ public class MainMenuScreen extends ScreenAdapter {
 	private boolean helppage = false;
 	private boolean helppage1 = false;
 	private boolean helppage2 = false;
+	private boolean helppage3 = false;
 
 	public MainMenuScreen(final MazeGame game) {
 		this.game = game;
@@ -57,16 +62,19 @@ public class MainMenuScreen extends ScreenAdapter {
 		BrewardTex = new TextureRegion(new Texture("dinosaur_egg.png"));
 		RpunishmentTex = new TextureRegion(new Texture("baby_dinosaur.png"));
 		BpunishmentTex = new TextureRegion(new Texture("alien.png"));
-		
+        movingEnemyTex = new TextureRegion(new Texture("DinoSprite.png"),4,1,17,17);
+        patrollingEnemeyTex = new TextureRegion(new Texture("ptero.png"), 0,0,31,16);
+        
 		stage1 = new Stage(new ScreenViewport());
 		stage2 = new Stage(new ScreenViewport());
 		stage3 = new Stage(new ScreenViewport());
+		stage4 = new Stage(new ScreenViewport());
 		
+		skin = new Skin(Gdx.files.internal("skin-soldier/star-soldier-ui.json"));
 		
 		change_x = -150;
 		change_y = -60;
 		
-		skin = new Skin(Gdx.files.internal("skin-soldier/star-soldier-ui.json"));
 		startbutton = new TextButton("Start", skin);
 		startbutton.setSize(Gdx.graphics.getWidth() / 10 * 2, Gdx.graphics.getHeight() / 10);
 		startbutton.setPosition(middle_x + change_x, middle_y + change_y);
@@ -79,7 +87,6 @@ public class MainMenuScreen extends ScreenAdapter {
 					}
 			
 		});
-		stage1.addActor(startbutton);
 		
 		change_x = -100;
 		change_y = -220;
@@ -96,7 +103,6 @@ public class MainMenuScreen extends ScreenAdapter {
 					}
 			
 		});
-		stage1.addActor(exitbutton);
 		
 		change_x = -150;
 		change_y = -140;
@@ -113,15 +119,9 @@ public class MainMenuScreen extends ScreenAdapter {
 					}
 			
 		});
-		stage1.addActor(helpbutton);
-		
-		change_x = -560;
-		change_y = -300;
 		
 		helppage1button = new TextButton("Page 1", skin);
 		helppage1button.setSize(Gdx.graphics.getWidth() / 10 * 2, Gdx.graphics.getHeight() / 10);
-		helppage1button.setPosition(middle_x + change_x, middle_y + change_y);
-		
 		// listener for touch button
 		helppage1button.addListener(new ChangeListener() {
 					public void changed(ChangeEvent event, Actor actor) {
@@ -130,25 +130,32 @@ public class MainMenuScreen extends ScreenAdapter {
 					}
 			
 		});
-		stage3.addActor(helppage1button);
-		
-		change_x = 300;
 		
 		helppage2button = new TextButton("Page 2", skin);
 		helppage2button.setSize(Gdx.graphics.getWidth() / 10 * 2, Gdx.graphics.getHeight() / 10);
-		helppage2button.setPosition(middle_x + change_x, middle_y + change_y);
-		
 		// listener for touch button
 		helppage2button.addListener(new ChangeListener() {
 					public void changed(ChangeEvent event, Actor actor) {
 						helppage1 = false;
 						helppage2 = true;
+						helppage3 = false;
 					}
 			
 		});
-		stage2.addActor(helppage2button);
+		
+		helppage3button = new TextButton("Page 2", skin);
+		helppage3button.setSize(Gdx.graphics.getWidth() / 10 * 2, Gdx.graphics.getHeight() / 10);
+		// listener for touch button
+		helppage3button.addListener(new ChangeListener() {
+					public void changed(ChangeEvent event, Actor actor) {
+						helppage2 = false;
+						helppage3 = true;
+					}
+			
+		});
 		
 		change_x = -140;
+		change_y = -300;
 		
 		helpbackbutton = new TextButton("Back", skin);
 		helpbackbutton.setSize(Gdx.graphics.getWidth() / 10 * 2, Gdx.graphics.getHeight() / 10);
@@ -158,11 +165,12 @@ public class MainMenuScreen extends ScreenAdapter {
 		helpbackbutton.addListener(new ChangeListener() {
 					public void changed(ChangeEvent event, Actor actor) {
 						helppage = false;
+						helppage1 = false;
 						helppage2 = false;
+						helppage3 = false;
 					}
 			
 		});
-		stage3.addActor(helpbackbutton);
 	}
 
 	public void render(float delta) {
@@ -170,16 +178,28 @@ public class MainMenuScreen extends ScreenAdapter {
 		ScreenUtils.clear(Color.BLACK);
 		
 		if(helppage) {
+			
+			batch.begin();
+			
+			change_x = -180;
+			change_y = 300;
+			
+			batch.draw(backgroundTexture, 0, 0);
+			font.getData().setScale(2, 2);
+			font.draw(batch, "How to play", middle_x + change_x, middle_y + change_y);
+			
+			batch.end();
+			
 			if(helppage1) {
 				Gdx.input.setInputProcessor(stage2);
-				batch.begin();
+				change_y = -300;
 				
-				change_x = -180;
-				change_y = 300;
-				//draw text. Remember that x and y are in meters
-				batch.draw(backgroundTexture, 0, 0);
-				font.getData().setScale(2, 2);
-				font.draw(batch, "How to play", middle_x + change_x, middle_y + change_y);
+				stage2.addActor(helpbackbutton);
+				change_x = 300;
+				helppage2button.setPosition(middle_x + change_x, middle_y + change_y);
+				stage2.addActor(helppage2button);
+				
+				batch.begin();
 				
 				change_x = -(middle_x) + 30;
 				change_y = 200;
@@ -202,14 +222,17 @@ public class MainMenuScreen extends ScreenAdapter {
 			}
 			else if(helppage2) {
 				Gdx.input.setInputProcessor(stage3);
-				batch.begin();
+				change_y = -300;
 				
-				change_x = -180;
-				change_y = 300;
-				//draw text. Remember that x and y are in meters
-				batch.draw(backgroundTexture, 0, 0);
-				font.getData().setScale(2, 2);
-				font.draw(batch, "How to play", middle_x + change_x, middle_y + change_y);
+				stage3.addActor(helpbackbutton);
+				change_x = -560;
+				helppage1button.setPosition(middle_x + change_x, middle_y + change_y);
+				stage3.addActor(helppage1button);
+				change_x = 300;
+				helppage3button.setPosition(middle_x + change_x, middle_y + change_y);
+				stage3.addActor(helppage3button);
+				
+				batch.begin();
 				
 				change_x = -(middle_x) + 30;
 				change_y = 120;
@@ -246,43 +269,65 @@ public class MainMenuScreen extends ScreenAdapter {
 				
 				font.draw(batch, ": Bonus Punishment, collect will loss 10 point, appear on map in fix time", middle_x + change_x, middle_y + change_y);
 				
-				
-				/*
-				change_x = -(middle_x) + 50;
-				change_y = 200;
-				font.getData().setScale(1,1);;
-				font.draw(batch, "Movement: Use 'W', 'A', 'S', 'D' to move in the game page 2", middle_x + change_x, middle_y + change_y);
-				
-				change_y = 100;
-				font.draw(batch, "Stop: Use 'Ese' can stop in the game", middle_x + change_x, middle_y + change_y);
-				
-				change_y = 0;
-				font.draw(batch, "Win the Game: Collect all the rewards show on the board and go to the end block", middle_x + change_x, middle_y + change_y);
-				
-				change_y = -100;
-				font.draw(batch, "Loss the Game: Monster catch you or point lower than 0", middle_x + change_x, middle_y + change_y);
-				*/
 				batch.end();
 				
 				stage3.act();
 				stage3.draw();
 			}
+			else if(helppage3) {
+				Gdx.input.setInputProcessor(stage4);
+				change_y = -300;
+				
+				stage4.addActor(helpbackbutton);
+				change_x = -560;
+				helppage2button.setPosition(middle_x + change_x, middle_y + change_y);
+				stage4.addActor(helppage2button);
+				
+				batch.begin();
+				
+				change_x = -(middle_x) + 30;
+				change_y = 120;
+				
+				batch.draw(movingEnemyTex, middle_x + change_x, middle_y + change_y, Gdx.graphics.getWidth()/10, Gdx.graphics.getHeight()/10);
+				
+				change_y = -80;
+				
+				batch.draw(patrollingEnemeyTex, middle_x + change_x, middle_y + change_y, Gdx.graphics.getWidth()/10, Gdx.graphics.getHeight()/10);
+				
+				change_x = -(middle_x) + 210;
+				change_y = 170;
+				
+				font.getData().setScale(1,1);
+				font.draw(batch, ": This is the mighty T.rex.\n  He will hunt you.", middle_x + change_x, middle_y + change_y);
+				
+				change_y = -30;
+
+                font.draw(batch, ": The Pterodactyl is mighty territorial\n  Avoid his set path.", middle_x + change_x, middle_y + change_y);
+		
+				batch.end();
+				
+				stage4.act();
+				stage4.draw();
+			}
 		}
 		else {
 			Gdx.input.setInputProcessor(stage1);
+			stage1.addActor(startbutton);
+			stage1.addActor(helpbutton);
+			stage1.addActor(exitbutton);
+			
 			
 			batch.begin();
 			
 			change_x = -100;
 			change_y = 150;
-			//draw text. Remember that x and y are in meters
+			
 			batch.draw(backgroundTexture, 0, 0);
 			font.getData().setScale(2, 2);
 			font.draw(batch, "Game", middle_x + change_x, middle_y + change_y);
 			
 			batch.end();
 			
-			//draw the button
 			stage1.act();
 			stage1.draw();
 			
@@ -309,5 +354,7 @@ public class MainMenuScreen extends ScreenAdapter {
 		skin.dispose();
 		stage1.dispose();
 		stage2.dispose();
+		stage3.dispose();
+		stage4.dispose();
 	}
 }
