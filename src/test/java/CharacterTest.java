9b3b49ef8c342d70d36376deab3bf794474ad6c3
Rodrigo Arce Diaz <ria12@sfu.ca>
2024-11-13 17:39:32 -0800

    Added the initial workings for testing the character

---
+++ src/test/java/CharacterTest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import entities.Character;
+import board.Board;
+import board.Block;
+
+public class CharacterTest {
+
+    private Character character;
+    private Board board;
+    private Block block;
+
+    /**
+     *
+     */
+    @BeforeEach
+    public void setUp() {
+
+        TextureRegion mockTextureRegion = mock(TextureRegion.class);
+
+        board = mock(Board.class);
+        block = mock(Block.class);
+
+        character = new Character(mockTextureRegion, 1, 1);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void initialPositionTest() {
+        assertEquals(1, character.getX());
+        assertEquals(1, character.getY());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void moveUpTest() {
+        when(board.getBlock(anyInt(), anyInt())).thenReturn(block);
+        when(block.enter()).thenReturn(true);
+        character.direction('W', board);
+        assertEquals(1, character.getX());
+        assertEquals(2, character.getY());
+    }
+}
