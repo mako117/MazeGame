c0950aa8c9f367c115c4f423c76f71bba7381703
Caleb Hengeveld <crh14@sfu.ca>
2024-11-16 13:43:21 -0800

    Adding Tom Grill's skeleton for LibGDX testing

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,18 @@
-public class GameTest {
-}
+// import static org.junit.jupiter.api.Assertions.*;
+// import static org.mockito.Mockito.*;
+
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.*;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.junit.runner.RunWith;
+// import org.mockito.Mock;
+
+// @RunWith(GdxTestRunner.class)
+// public class GameTest {
+
+//     @BeforeEach
+//     public void setup() {
+
+//     }
+// }
