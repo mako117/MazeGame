aac29f0ddfeab86d8a218e59670b324a71275c88
Alicia Lam <alicia_lam_2@sfu.ca>
2024-11-16 13:17:27 -0800

    Create board test file

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,39 @@
+import board.Board;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class BoardTest {
+
+    // Test individual functions of board class here.
+
+    private Board mockBoard;
+
+
+    @BeforeEach
+    void setup(){
+        mockBoard = mock(Board.class);
+    }
+
+
+    @Test
+    void barrierBlockEnter(){
+
+    }
+
+    @Test
+    void blockEnter(){
+
+    }
+
+    @Test
+    void boomBlockEnter(){
+
+    }
+
+    @Test
+    void wallEnter(){
+
+    }
+}
