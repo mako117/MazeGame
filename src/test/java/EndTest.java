612e1c05cc0c38a9dde416fa2ddf8c0d134b6d77
Caleb Hengeveld <crh14@sfu.ca>
2024-11-16 17:05:59 -0800

    Unit test file skeletons for all the screens created

---
+++ src/test/java/EndTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import screens.EndScreen;
+
+@RunWith(GdxTestRunner.class)
+public class EndTest {
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    /**
+     * Test to see if the Restart button works when left-clicked on by the mouse.
+     */
+    @Test
+    public void restartButtonWorks() {
+    }
+
+    /**
+     * Test to see if the Try Again button works when left-clicked on by the mouse.
+     */
+    @Test
+    public void tryAgainButtonWorks() {
+    }
+
+    /**
+     * Test to see if the exit button works when left-clicked on by the mouse.
+     */
+    @Test
+    public void exitButtonWorks() {
+    }
+}
