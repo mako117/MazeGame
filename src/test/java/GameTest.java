a61186ff54714fdf4daa4bd4ab5543be3e643de2
Caleb Hengeveld <crh14@sfu.ca>
2024-11-16 17:26:18 -0800

    keep unfinished test files completely commented out until they are ready

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,71 +1,71 @@
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+// import static org.junit.jupiter.api.Assertions.*;
+// import static org.mockito.Mockito.*;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import screens.GameScreen;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.*;
+// import org.junit.jupiter.api.Test;
+// import org.junit.runner.RunWith;
+// import org.mockito.Mock;
+// import screens.GameScreen;
 
-@RunWith(GdxTestRunner.class)
-public class GameTest {
+// @RunWith(GdxTestRunner.class)
+// public class GameTest {
 
-    @BeforeEach
-    public void setup() {
+//     @BeforeEach
+//     public void setup() {
 
-    }
+//     }
 
-    /**
-     * Test to see if the pause button works when left-clicked on by the mouse.
-     */
-    @Test
-    public void pauseButtonWorks() {
-    }
+//     /**
+//      * Test to see if the pause button works when left-clicked on by the mouse.
+//      */
+//     @Test
+//     public void pauseButtonWorks() {
+//     }
 
-    /**
-     * Test to see if the pause button works when pressing Esc.
-     */
-    @Test
-    public void escToPauseWorks() {
-    }
+//     /**
+//      * Test to see if the pause button works when pressing Esc.
+//      */
+//     @Test
+//     public void escToPauseWorks() {
+//     }
 
-    /**
-     * Test to see if the resume button works when left-clicked on by the mouse.
-     */
-    @Test
-    public void resumeButtonWorks() {
-    }
+//     /**
+//      * Test to see if the resume button works when left-clicked on by the mouse.
+//      */
+//     @Test
+//     public void resumeButtonWorks() {
+//     }
 
-    /**
-     * Test to see if the resume button works when pressing Esc.
-     */
-    @Test
-    public void escToResumeWorks() {
-    }
+//     /**
+//      * Test to see if the resume button works when pressing Esc.
+//      */
+//     @Test
+//     public void escToResumeWorks() {
+//     }
 
-    /**
-     * Test to see if the help button works when left-clicked on by the mouse.
-     */
-    @Test
-    public void helpButtonWorks() {
+//     /**
+//      * Test to see if the help button works when left-clicked on by the mouse.
+//      */
+//     @Test
+//     public void helpButtonWorks() {
 
-    }
+//     }
 
-    /**
-     * Test to see if the restart button works when left-clicked on by the mouse.
-     */
-    @Test
-    public void restartButtonWorks() {
+//     /**
+//      * Test to see if the restart button works when left-clicked on by the mouse.
+//      */
+//     @Test
+//     public void restartButtonWorks() {
 
-    }
+//     }
 
-    /**
-     * Test to see if the exit button works when left-clicked on by the mouse.
-     */
-    @Test
-    public void exitButtonWorks() {
+//     /**
+//      * Test to see if the exit button works when left-clicked on by the mouse.
+//      */
+//     @Test
+//     public void exitButtonWorks() {
 
-    }
+//     }
 
-}
+// }
--- src/test/java/MainMenuTest.java
+++ src/test/java/MainMenuTest.java
@@ -1,38 +1,38 @@
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+// import static org.junit.jupiter.api.Assertions.*;
+// import static org.mockito.Mockito.*;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import screens.MainMenuScreen;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.*;
+// import org.junit.jupiter.api.Test;
+// import org.junit.runner.RunWith;
+// import org.mockito.Mock;
+// import screens.MainMenuScreen;
 
-@RunWith(GdxTestRunner.class)
-public class MainMenuTest {
-    @BeforeEach
-    public void setup() {
+// @RunWith(GdxTestRunner.class)
+// public class MainMenuTest {
+//     @BeforeEach
+//     public void setup() {
 
-    }
+//     }
 
-    /**
-     * Test to see if the start button works when left-clicked on by the mouse.
-     */
-    @Test
-    public void startButtonWorks() {
-    }
+//     /**
+//      * Test to see if the start button works when left-clicked on by the mouse.
+//      */
+//     @Test
+//     public void startButtonWorks() {
+//     }
 
-    /**
-     * Test to see if the help button works when left-clicked on by the mouse.
-     */
-    @Test
-    public void helpButtonWorks() {
-    }
+//     /**
+//      * Test to see if the help button works when left-clicked on by the mouse.
+//      */
+//     @Test
+//     public void helpButtonWorks() {
+//     }
 
-    /**
-     * Test to see if the exit button works when left-clicked on by the mouse.
-     */
-    @Test
-    public void exitButtonWorks() {
-    }
-}
+//     /**
+//      * Test to see if the exit button works when left-clicked on by the mouse.
+//      */
+//     @Test
+//     public void exitButtonWorks() {
+//     }
+// }
