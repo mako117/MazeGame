ea9970a8843e28f781ed7a7b2eb7bbc4a315a466
Alicia Lam <alicia_lam_2@sfu.ca>
2024-11-18 01:44:51 -0800

    Add test methods for board class

--- src/test/java/board/BoardTest.java
+++ src/test/java/board/BoardTest.java
@@ -1,51 +1,96 @@
 package board;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.mockito.Mockito.mock;
 
-public class BoardTest {
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
 
-    private Board mockBoard;
 
 
-    @BeforeEach
-    void setup(){
 
+/**
+ * Create context for libgdx components in board. T-T <br>
+ * By extending this in our test class, we will override the create method, allowing Gdx textures to be initialized. <br>
+ * Solution from: <a href="https://www.reddit.com/r/libgdx/comments/1by4jgn/loading_assets_for_tests_in_libgdx/">Loading assets for tests in libgdx?</a>
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+abstract class AbstractTestWithHeadlessGdxContext extends ApplicationAdapter {
+    HeadlessApplication application;
+
+    AbstractTestWithHeadlessGdxContext() {
+        HeadlessApplicationConfiguration conf = new HeadlessApplicationConfiguration();
+        application = new HeadlessApplication(this, conf);
+        Gdx.gl = mock(GL20.class);
     }
 
+    @Override
+    public void render() {
+        // no-op, prevents exception when trying to render since we are using a headless application
+    }
 
+    @AfterAll
+    void afterAll() {
+        application.exit();
+    }
+}
+
+
+
+public class BoardTest extends AbstractTestWithHeadlessGdxContext{
+
+    Board board;
+
+    @BeforeAll
+    public void setup(){
+        board = new Board();
+    }
+
+    // Test block features
+    /**
+     * Test barrier block enter method.
+     */
     @Test
     void barrierBlockEnterTest(){
         BarrierBlock b = new BarrierBlock();
         BarrierBlock b2 = new BarrierBlock(0, 0, null);
 
-        assertEquals(false, b.enter());
-        assertEquals(false, b2.enter());
+        assertFalse(b.enter());
+        assertFalse(b2.enter());
     }
 
-
+    /**
+     * Test room block enter method.
+     */
     @Test
     void roomBlockEnterTest(){
         RoomBlock b = new RoomBlock();
         RoomBlock b2 = new RoomBlock(0, 0, null);
 
-        assertEquals(true, b.enter());
-        assertEquals(true, b2.enter());
+        assertTrue(b.enter());
+        assertTrue(b2.enter());
     }
 
+    /**
+     * Test wall enter method.
+     */
     @Test
     void wallEnterTest(){
         Wall w = new Wall();
         Wall w2 = new Wall(0, 0, null);
 
-        assertEquals(false, w.enter());
-        assertEquals(false, w2.enter());
+        assertFalse(w.enter());
+        assertFalse(w2.enter());
     }
 
+    /**
+     * Test block position getter.
+     */
     @Test
     void blockPositionTest(){
         Block b = new RoomBlock(0,1, null);
@@ -59,5 +104,103 @@ public class BoardTest {
         assertEquals(1, b.getXPosition());
         assertEquals(2, b.getYPosition());
 
+    }
+
+
+    // Test board features
+
+    /**
+     * Test get start block method.
+     */
+    @Test
+    void getStart() {
+        Block start = board.getStart();
+
+        assertEquals(start.getXPosition(), 1);
+        assertEquals(start.getYPosition(), 1);
+    }
+
+    /**
+     * Test get end block method.
+     */
+    @Test
+    void getEnd() {
+        Block end = board.getEnd();
+
+        assertEquals(end.getXPosition(), 1);
+        assertEquals(end.getYPosition(), 21);
+    }
+
+    /**
+     * Test get total regular reward count.
+     */
+    @Test
+    void getTotalRegRewardCnt() {
+    }
+
+    /**
+     * Test get block at coordinates x y.
+     */
+    @Test
+    void getBlock() {
+        int x = 5;
+        int y = 5;
+        Block block = board.getBlock(x, y);
+
+        assertEquals(block.getXPosition(), x);
+        assertEquals(block.getYPosition(), y);
+
+    }
+
+
+    /**
+     * Test get board width method.
+     */
+    @Test
+    void getWidth() {
+        assertEquals(board.getWidth(), 23);
+    }
+
+    /**
+     * Test get board height method.
+     */
+    @Test
+    void getHeight() {
+        assertEquals(board.getHeight(), 23);
+    }
+
+    /**
+     * Test collect regular reward.         // TODO: complete the rest
+     */
+    @Test
+    void regRewardCollect() {
+    }
+
+    /**
+     * Test collect bonus reward.
+     */
+    @Test
+    void bonRewardCollect() {
+    }
+
+    /**
+     * Test collect regular punishment.
+     */
+    @Test
+    void regPunishmentCollect() {
+    }
+
+    /**
+     * Test collect bonus punishment.
+     */
+    @Test
+    void bonPunishmentCollect() {
+    }
+
+    /**
+     * Test generate new bonus rewards.
+     */
+    @Test
+    void genNewBonus() {
     }
 }
