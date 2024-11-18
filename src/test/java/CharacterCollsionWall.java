8dbbe157c1c215a2cc42a8c639877ec19a2ab11b
Shiqi Zhong <sza181@sfu.ca>
2024-11-19 06:00:17 +0800

    test for character collsion for wall and roomblock

---
+++ src/test/java/CharacterCollsionWall.java
@@ -0,0 +1,125 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import board.Wall;
+import board.Block;
+import board.RoomBlock;
+import entities.Character;
+import board.Board;
+
+class CharacterCollsionWall {
+	private TextureRegion tex = mock(TextureRegion.class);
+	private Board mockboard = mock(Board.class);
+	
+	//Wall
+	@Test
+	void moveupwithwall() {
+		Wall w = new Wall(1,2,tex);
+		when(mockboard.getBlock(anyInt(), anyInt())).thenReturn(w);
+		
+		Character c = new Character(tex);
+		
+		c.direction('W', mockboard);
+		
+		assertEquals(1,c.getX());
+		assertEquals(1,c.getY());
+	}
+	
+	@Test
+	void moveleftwithwall() {
+		Wall w = new Wall(0,1,tex);
+		when(mockboard.getBlock(anyInt(), anyInt())).thenReturn(w);
+		
+		Character c = new Character(tex);
+		
+		c.direction('A', mockboard);
+		
+		assertEquals(1,c.getX());
+		assertEquals(1,c.getY());
+	}
+
+	@Test
+	void moverightwithwall() {
+		Wall w = new Wall(2,1,tex);
+		when(mockboard.getBlock(anyInt(), anyInt())).thenReturn(w);
+		
+		Character c = new Character(tex);
+		
+		c.direction('D', mockboard);
+		
+		assertEquals(1,c.getX());
+		assertEquals(1,c.getY());
+	}
+	
+	@Test
+	void movedownwithwall() {
+		Wall w = new Wall(1,0,tex);
+		when(mockboard.getBlock(anyInt(), anyInt())).thenReturn(w);
+		
+		Character c = new Character(tex);
+		
+		c.direction('S', mockboard);
+		
+		assertEquals(1,c.getX());
+		assertEquals(1,c.getY());
+	}
+	
+	//RoomBlock
+	@Test
+	void moveupwithroomblock() {
+		RoomBlock b = new RoomBlock(1,2,tex);
+		when(mockboard.getBlock(anyInt(), anyInt())).thenReturn(b);
+		
+		Character c = new Character(tex);
+		
+		c.direction('W', mockboard);
+		
+		assertEquals(1,c.getX());
+		assertEquals(2,c.getY());
+	}
+	
+	@Test
+	void moveleftwithroomblock() {
+		RoomBlock b = new RoomBlock(0,1,tex);
+		when(mockboard.getBlock(anyInt(), anyInt())).thenReturn(b);
+		
+		Character c = new Character(tex);
+		
+		c.direction('A', mockboard);
+		
+		assertEquals(0,c.getX());
+		assertEquals(1,c.getY());
+	}
+
+	@Test
+	void moverightwithroomblock() {
+		RoomBlock b = new RoomBlock(2,1,tex);
+		when(mockboard.getBlock(anyInt(), anyInt())).thenReturn(b);
+		
+		Character c = new Character(tex);
+		
+		c.direction('D', mockboard);
+		
+		assertEquals(2,c.getX());
+		assertEquals(1,c.getY());
+	}
+	
+	@Test
+	void movedownwithroomblock() {
+		RoomBlock b = new RoomBlock(1,0,tex);
+		when(mockboard.getBlock(anyInt(), anyInt())).thenReturn(b);
+		
+		Character c = new Character(tex);
+		
+		c.direction('S', mockboard);
+		
+		assertEquals(1,c.getX());
+		assertEquals(0,c.getY());
+	}
+}
