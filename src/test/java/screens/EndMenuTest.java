eae8ca1b800201fef6ab90c67069908166130b99
Caleb Hengeveld <crh14@sfu.ca>
2024-11-21 15:09:52 -0800

    main menu and end tests work, but you have to close the windows manually

---
+++ src/test/java/screens/EndMenuTest.java
@@ -0,0 +1,64 @@
+package screens;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
+import org.junit.Rule;
+import org.junit.jupiter.api.*;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import org.junit.rules.ExpectedException;
+
+import screens.*;
+import screens.MazeGame;
+
+public class EndMenuTest extends AbstractTestWithHeadlessGdxContext {
+
+    @Test
+    public void integrationTest() {
+        Runnable r = () -> {
+            try{ Thread.sleep(500); } catch(Exception e) {};
+        };
+        TestGame endGame = new TestGame(GameState.EndMenu, r);
+        application = new Lwjgl3Application(endGame, config);
+        // only buttons and no key input, so nothing to test except that it is an EndScreen object
+        assertEquals(true, (endGame.getCurrentScreen() instanceof EndScreen));
+    }
+
+    // // TODO: get the headless backend working; the current problem is getting OpenGL methods working
+    // // assume the setup gets us to the Game Over screen.
+    // @BeforeEach
+    // public void setup() {
+    // }
+
+    // /**
+    //  * Test to see if the Play Again button works.
+    //  */
+    // @Test
+    // public void playAgainButtonWorks() {
+    // }
+
+    // /**
+    //  * Test to see if the Try Again button works.
+    //  */
+    // @Test
+    // public void tryAgainButtonWorks() {
+    // }
+
+    // /**
+    //  * Test to see if the exit button works.
+    //  */
+    // @Test
+    // public void exitButtonWorks() {
+    // }
+}
