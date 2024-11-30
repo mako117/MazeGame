4113bf73883041a93caa236620862a284a901cb0
Caleb Hengeveld <crh14@sfu.ca>
2024-11-30 15:15:54 -0800

    Refactored movement selection if-else statements for when moving along the x axis is preferred in find_player into its own wantsToMoveAlongX method.

--- src/main/java/entities/enemy/Moving_Enemies.java
+++ src/main/java/entities/enemy/Moving_Enemies.java
@@ -35,130 +35,11 @@ public class Moving_Enemies extends Enemies {
 
         if(moveYDistance < moveXDistance) {
             return wantsToMoveAlongY(xDistance, yDistance, gameBoard);
-        }
-        // } else {
-        //     return wantsToMoveAlongX(xDistance, yDistance);
-        // }
-
-        /*
-        If moving along the y-axis will get the enemy closer to the player than moving
-        along the x-axis will, the code
-            1. Tries moving towards the player along the y-axis
-            2. Tries moving towards the player along the x-axis
-            3. Tries moving away from the player along the x-axis
-            4. Moves away from the player along the y-axis
-        If the enemy is lined up with the player on an axis, it will not move along that axis; 
-        therefore once its x and y coordinates match those of the player, it will not move.
-        This code assumes it is impossible for the enemy to have been boxed in on all sides.
-        */
-        // if(moveYDistance < moveXDistance) {
-        //     if(yDistance < 0) { // wants to move up
-        //         if(gameBoard.getBlock(this.getX(), this.getY() + 1).enter() == true) {
-        //             return 'W';
-        //         } else if(xDistance < 0) { // can't move up, so wants to move right
-        //             if(gameBoard.getBlock(this.getX() + 1, this.getY()).enter() == true) {
-        //                 return 'D';
-        //             } else if(gameBoard.getBlock(this.getX() - 1, this.getY()).enter() == true) { // can't move right, so tries left
-        //                 return 'A';
-        //             } else { // moves down
-        //                 return 'S';
-        //             }
-        //         } else if(xDistance > 0) { // can't move up, so wants to move left
-        //             if(gameBoard.getBlock(this.getX() - 1, this.getY()).enter() == true) {
-        //                 return 'A';
-        //             } else if(gameBoard.getBlock(this.getX() + 1, this.getY()).enter() == true) { // can't move left, so tries right
-        //                 return 'D';
-        //             } else { // moves down
-        //                 return 'S';
-        //             }
-        //         } else { // doesn't want to move on x-axis, can't move up, and moving down makes it worse; therefore it doesn't move
-        //             return 'I';
-        //         }
-        //     }
-        //     else /*if (yDistance > 0) */ { // wants to move down
-        //         if(gameBoard.getBlock(this.getX(), this.getY() - 1).enter() == true) {
-        //             return 'S';
-        //         } else if(xDistance < 0) { // can't move down, so wants to move right
-        //             if(gameBoard.getBlock(this.getX() + 1, this.getY()).enter() == true) {
-        //                 return 'D';
-        //             } else if(gameBoard.getBlock(this.getX() - 1, this.getY()).enter() == true) {
-        //                 return 'A';
-        //             } else {
-        //                 return 'W';
-        //             }
-        //         } else if(xDistance > 0) { // can't move down, so wants to move left
-        //             if(gameBoard.getBlock(this.getX() - 1, this.getY()).enter() == true) {
-        //                 return 'A';
-        //             } else if(gameBoard.getBlock(this.getX() + 1, this.getY()).enter() == true) {
-        //                 return 'D';
-        //             } else {
-        //                 return 'W';
-        //             }
-        //         } else { // doesn't want to move on x-axis, can't move down, and moving up makes it worse; therefore it doesn't move
-        //             return 'I';
-        //         }
-        //     }
-        // }
-        // /*
-        // If moving along the x-axis will get the enemy closer to the player than moving
-        // along the y-axis will, the code
-        //     1. Tries moving towards the player along the x-axis
-        //     2. Tries moving towards the player along the y-axis
-        //     3. Tries moving away from the player along the y-axis
-        //     4. Moves away from the player along the x-axis
-        // If the enemy is lined up with the player on an axis, it will not move along that axis; 
-        // therefore once its x and y coordinates match those of the player, it will not move.
-        // This code assumes it is impossible for the enemy to have been boxed in on all sides.
-        // */
-        else {
-            if(xDistance < 0) { // wants to move right
-                if(gameBoard.getBlock(this.getX() + 1, this.getY()).enter() == true) {
-                    return 'D';
-                } else if(yDistance < 0) { // can't move right, so wants to move up
-                    if(gameBoard.getBlock(this.getX(), this.getY() + 1).enter() == true) {
-                        return 'W';
-                    } else if(gameBoard.getBlock(this.getX(), this.getY() - 1).enter() == true) {
-                        return 'S';
-                    } else {
-                        return 'A';
-                    }
-                } else if(yDistance > 0) { // can't move right, so wants to move down
-                    if(gameBoard.getBlock(this.getX(), this.getY() - 1).enter() == true) {
-                        return 'S';
-                    } else if(gameBoard.getBlock(this.getX(), this.getY() + 1).enter() == true) {
-                        return 'W';
-                    } else {
-                        return 'A';
-                    }
-                } else {
-                    return 'I';
-                }
-            }
-            else /* if (xDistance > 0) */ { // wants to move left
-                if(gameBoard.getBlock(this.getX() - 1, this.getY()).enter() == true) {
-                    return 'A';
-                } else if(yDistance < 0) { // can't move left, so wants to move up
-                    if(gameBoard.getBlock(this.getX(), this.getY() + 1).enter() == true) {
-                        return 'W';
-                    } else if(gameBoard.getBlock(this.getX(), this.getY() - 1).enter() == true) {
-                        return 'S';
-                    } else {
-                        return 'D';
-                    }
-                } else if (yDistance > 0) { // can't move left, so wants to move down
-                    if(gameBoard.getBlock(this.getX(), this.getY() - 1).enter() == true) {
-                        return 'S';
-                    } else if(gameBoard.getBlock(this.getX(), this.getY() + 1).enter() == true) {
-                        return 'W';
-                    } else {
-                        return 'D';
-                    }
-                } else {
-                    return 'I';
-                }
-            }
-        }        
+        } else {
+            return wantsToMoveAlongX(xDistance, yDistance, gameBoard);
+        }       
     }
+    
 
     /**
      * Calculates the distance from the moving enemy to the character if the enemy moves along the x-axis and if it moves along the y-axis.
@@ -191,6 +72,87 @@ public class Moving_Enemies extends Enemies {
         return array;
     }
 
+    /**
+     * If moving along the x-axis will get the enemy closer to the player than moving
+     * along the y-axis will, the code
+     *      1. Tries moving towards the player along the x-axis
+     *      2. Tries moving towards the player along the y-axis
+     *      3. Tries moving away from the player along the y-axis
+     *      4. Moves away from the player along the x-axis
+     * If the enemy is lined up with the player on an axis, it will not move along that axis;
+     * therefore once its x and y coordinates match those of the player, it will not move.
+     * This code assumes it is impossible for the enemy to have been boxed in on all sides.
+     * @param xDistance The enemy's <x>-coordinate - the character's <x>-coordinate.
+     * @param yDistance The enemy's <y>-coordinate - the character's <y>-coordinate.
+     * @param gameBoard The game board.
+     * @return  One of the input characters 'W', 'A', 'S', 'D', or 'I', corresponding to the directions
+     * North, West, South, East, and None.
+     */
+    private char wantsToMoveAlongX(int xDistance, int yDistance, Board gameBoard) {
+        if(xDistance < 0) { // wants to move right
+            if(gameBoard.getBlock(this.getX() + 1, this.getY()).enter() == true) {
+                return 'D';
+            } else if(yDistance < 0) { // can't move right, so wants to move up
+                if(gameBoard.getBlock(this.getX(), this.getY() + 1).enter() == true) {
+                    return 'W';
+                } else if(gameBoard.getBlock(this.getX(), this.getY() - 1).enter() == true) {
+                    return 'S';
+                } else {
+                    return 'A';
+                }
+            } else if(yDistance > 0) { // can't move right, so wants to move down
+                if(gameBoard.getBlock(this.getX(), this.getY() - 1).enter() == true) {
+                    return 'S';
+                } else if(gameBoard.getBlock(this.getX(), this.getY() + 1).enter() == true) {
+                    return 'W';
+                } else {
+                    return 'A';
+                }
+            } else {
+                return 'I';
+            }
+        }
+        else /* if (xDistance > 0) */ { // wants to move left
+            if(gameBoard.getBlock(this.getX() - 1, this.getY()).enter() == true) {
+                return 'A';
+            } else if(yDistance < 0) { // can't move left, so wants to move up
+                if(gameBoard.getBlock(this.getX(), this.getY() + 1).enter() == true) {
+                    return 'W';
+                } else if(gameBoard.getBlock(this.getX(), this.getY() - 1).enter() == true) {
+                    return 'S';
+                } else {
+                    return 'D';
+                }
+            } else if (yDistance > 0) { // can't move left, so wants to move down
+                if(gameBoard.getBlock(this.getX(), this.getY() - 1).enter() == true) {
+                    return 'S';
+                } else if(gameBoard.getBlock(this.getX(), this.getY() + 1).enter() == true) {
+                    return 'W';
+                } else {
+                    return 'D';
+                }
+            } else {
+                return 'I';
+            }
+        }
+    }
+
+    /**
+     * If moving along the y-axis will get the enemy closer to the player than moving
+     * along the x-axis will, the code
+     *      1. Tries moving towards the player along the y-axis
+     *      2. Tries moving towards the player along the x-axis
+     *      3. Tries moving away from the player along the x-axis
+     *      4. Moves away from the player along the y-axis
+     * If the enemy is lined up with the player on an axis, it will not move along that axis; 
+     * therefore once its x and y coordinates match those of the player, it will not move.
+     * This code assumes it is impossible for the enemy to have been boxed in on all sides.
+     * @param xDistance The enemy's <x>-coordinate - the character's <x>-coordinate.
+     * @param yDistance The enemy's <y>-coordinate - the character's <y>-coordinate.
+     * @param gameBoard The game board.
+     * @return  One of the input characters 'W', 'A', 'S', 'D', or 'I', corresponding to the directions
+     * North, West, South, East, and None.
+     */
     private char wantsToMoveAlongY(int xDistance, int yDistance, Board gameBoard) {
         if(yDistance < 0) { // wants to move up
             if(gameBoard.getBlock(this.getX(), this.getY() + 1).enter() == true) {
